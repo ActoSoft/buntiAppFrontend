006e8aca692e16b7d27a06ffd9744ee219f79f5b
Martin Melo <nitram-210397@hotmail.com>
2019-08-02 01:26:40 -0500

    Listen for the correct path

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.sendPushNotification = functions.firestore.document('usuarios/{id}/services/{docId}').onWrite((change, context) => {
+    // if (change.after.data().Accepted && change.after.data().WaitingTime) {
+
+    // }
+})
