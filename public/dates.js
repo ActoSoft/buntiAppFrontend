0f080e8b1932bb50458a5c7f3f8654bf4b68f8e1
Martin Melo <nitram-210397@hotmail.com>
2019-08-02 02:34:08 -0500

    Accept and decline refactor, working

--- public/dates.js
+++ public/dates.js
@@ -1,5 +1,4 @@
 var config = {
-  apiKey: "AIzaSyA_I_x2mljXdhCaCucr8VDS6EVssspryx0",
   authDomain: "buntiapp-82c84.firebaseapp.com",
   databaseURL: "https://buntiapp-82c84.firebaseio.com",
   projectId: "buntiapp-82c84",
@@ -15,48 +14,48 @@ var ViewList = document.getElementById('ListView');
 var ViewList2 = document.getElementById('ListView2');
 
 db.collection("usuarios").onSnapshot((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        db.collection("usuarios").doc(doc.id).collection("services").onSnapshot((querySnapshot) => {
-            ViewList.innerHTML = '';
-            ViewList2.innerHTML = '';
+    querySnapshot.forEach((docUser) => {
+        db.collection("usuarios").doc(docUser.id).collection("services").onSnapshot((querySnapshot) => {
+            // ViewList.innerHTML = '';
+            // ViewList2.innerHTML = '';
             var i=0;
-            querySnapshot.forEach((doc) => {
+            querySnapshot.forEach((docServices) => {
                 i++;
                 notify();
-                if (i%2){ console.log("Num "+i);
+                if (i%2){
                 ViewList.innerHTML += `<div class="global"><div class="default"><list class="list-group-item listStyle">
                 <table class="table">
-                        <tr class="tr"><td class="data">Usuario:</td><td><span class="data-info">${doc.data().usuario}</span></td></tr>
-                        <tr><td class="data">Fecha de solicitud:</td><td><span class="data-info">${doc.data().fecha}</span></td></tr>
-                        <tr><td class="data">Hora de solicitud:</td><td><span class="data-info">${doc.data().hora}</span></td></tr>
-                        <tr><td class="data">Forma de pago:</td><td><span class="data-info">${doc.data().pago}</span></td></tr>
+                        <tr class="tr"><td class="data">Usuario:</td><td><span class="data-info">${docServices.data().usuario}</span></td></tr>
+                        <tr><td class="data">Fecha de solicitud:</td><td><span class="data-info">${docServices.data().fecha}</span></td></tr>
+                        <tr><td class="data">Hora de solicitud:</td><td><span class="data-info">${docServices.data().hora}</span></td></tr>
+                        <tr><td class="data">Forma de pago:</td><td><span class="data-info">${docServices.data().pago}</span></td></tr>
                         <tr><td class="data-center" colspan="2">Localización:</td></tr>
-                        <tr><td colspan="2" class="data-place"><span>${doc.data().direccion}</span></td></tr>
+                        <tr><td colspan="2" class="data-place"><span>${docServices.data().direccion}</span></td></tr>
                     </table></div>
                 <div class="space">
-                <label class="code">${doc.id}</label>
+                <label class="code">${docServices.id}</label>
                 <div class="footer-content">
                 <button type="button" class="btn btn-info btn-sm"
-                data-toggle="modal" onclick="DataTime('${doc.id}')">Aceptar</button>
+                data-toggle="modal" onclick="DataTime('${docServices.id}')">Aceptar</button>
                 <button type="button" class="btn btn-danger btn-sm"
-                data-toggle="modal" onclick="DeleteList('${doc.id}')">Rechazar</button></div></div></list></div>`;}
-                else{ console.log("Num2 "+i);
+                data-toggle="modal" onclick="DeleteList('${docServices.id}')">Rechazar</button></div></div></list></div>`;}
+                else{
                 ViewList2.innerHTML += `<div class="global"><div class="default"><list class="list-group-item listStyle">
                 <table class="table">
-                        <tr class="tr"><td class="data">Usuario:</td><td><span class="data-info">${doc.data().usuario}</span></td></tr>
-                        <tr><td class="data">Fecha de solicitud:</td><td><span class="data-info">${doc.data().fecha}</span></td></tr>
-                        <tr><td class="data">Hora de solicitud:</td><td><span class="data-info">${doc.data().hora}</span></td></tr>
-                        <tr><td class="data">Forma de pago:</td><td><span class="data-info">${doc.data().pago}</span></td></tr>
+                        <tr class="tr"><td class="data">Usuario:</td><td><span class="data-info">${docServices.data().usuario}</span></td></tr>
+                        <tr><td class="data">Fecha de solicitud:</td><td><span class="data-info">${docServices.data().fecha}</span></td></tr>
+                        <tr><td class="data">Hora de solicitud:</td><td><span class="data-info">${docServices.data().hora}</span></td></tr>
+                        <tr><td class="data">Forma de pago:</td><td><span class="data-info">${docServices.data().pago}</span></td></tr>
                         <tr><td class="data-center" colspan="2">Localización:</td></tr>
-                        <tr><td colspan="2" class="data-place"><span>${doc.data().direccion}</span></td></tr>
+                        <tr><td colspan="2" class="data-place"><span>${docServices.data().direccion}</span></td></tr>
                     </table></div>
                 <div class="space">
-                <label class="code">${doc.id}</label>
+                <label class="code">${docServices.id}</label>
                 <div class="footer-content">
                 <button type="button" class="btn btn-info btn-sm"
-                data-toggle="modal" onclick="DataTime('${doc.id}')">Aceptar</button>
+                data-toggle="modal" onclick="DataTime('${docUser.id}', '${docServices.id}')">Aceptar</button>
                 <button type="button" class="btn btn-danger btn-sm"
-                data-toggle="modal" onclick="DeleteList('${doc.id}')">Rechazar</button></div></div></list></div>`; }   
+                data-toggle="modal" onclick="DeleteList('${docUser.id}', '${docServices.id}')">Rechazar</button></div></div></list></div>`; }
             });
         });
     });
@@ -64,61 +63,98 @@ db.collection("usuarios").onSnapshot((querySnapshot) => {
 
 
 // Delete List
-function DeleteList(id) {
+function DeleteList(userId, serviceId) {
+    console.log(userId)
+    console.log(serviceId)
     $('#DialogDelete').modal('show');
     $(document).ready(function(){
         $("#btn-delete").click(function(){
-            db.collection("usuarios").onSnapshot((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    db.collection("usuarios").doc(doc.id).collection("services").doc(id).delete().then(function() {
-                        console.log("Document successfully deleted!");
-                        $('#DialogDelete').modal('hide');
-                        $('#ToastC').toast('hide');
-                        $('#ToastD').toast('show');
-                    }).catch(function(error) {
-                        console.error("Error removing document: ", error);
-                    });
-                });
-           });
+        //     db.collection("usuarios").onSnapshot((querySnapshot) => {
+        //         querySnapshot.forEach((doc) => {
+        //             db.collection("usuarios").doc(doc.id).collection("services").doc(id).delete().then(function() {
+        //                 console.log("Document successfully deleted!");
+        //                 $('#DialogDelete').modal('hide');
+        //                 $('#ToastC').toast('hide');
+        //                 $('#ToastD').toast('show');
+        //             }).catch(function(error) {
+        //                 console.error("Error removing document: ", error);
+        //             });
+        //         });
+        //    });
+        const serviceRef = db.collection("usuarios").doc(userId).collection("services").doc(serviceId)
+        serviceRef.delete()
+            .then(function() {
+                $('#DialogDelete').modal('hide');
+                $('#ToastC').toast('hide');
+                $('#ToastD').toast('show');
+            })
+            .catch(function(error) {
+                console.error("Error removing document: ", error);
+            })
         });
-      }); }
+    });
+}
 
 // Update Info & Time
-function DataTime(id){
+function DataTime(userId, serviceId){
     $('#dialog').modal('show');
     $(document).ready(function(){
         $("#btn-save").click(function(){
-            db.collection("usuarios").onSnapshot((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    var washingtonRef = db.collection("usuarios").doc(doc.id).collection("services").doc(id);
-                    moment = new Date();
-                        var year = moment.getFullYear();
-                        var month = (moment.getMonth()+1);
-                        var day = moment.getDate();
-                        var hour = moment.getHours();
-                        var minute = moment.getMinutes();
-                        var second = moment.getSeconds();
-                        var exactTime = day+"/"+month+"/"+year+"  "+hour+":"+minute+":"+second;
-                        var timeNum = document.getElementById('time').value;
-                        var time = timeNum +" minutos";
-                        $('#dialog').modal('hide');
-                        return washingtonRef.update({
-                            Aceptado: true,
-                            Tiempo: exactTime,
-                            Minutos: time,
-                        })
-                    .then(function() {
-                        console.log("Document successfully updated!");
-                        document.getElementById('time').value = '';
-                    })
-                    .catch(function(error) {
-                        // The document probably doesn't exist.
-                        console.error("Error updating document: ", error);
-                    });
-                    });
-            });
+            // db.collection("usuarios").onSnapshot((querySnapshot) => {
+            //     querySnapshot.forEach((doc) => {
+            //         var washingtonRef = db.collection("usuarios").doc(doc.id).collection("services").doc(id);
+            //         moment = new Date();
+            //             var year = moment.getFullYear();
+            //             var month = (moment.getMonth()+1);
+            //             var day = moment.getDate();
+            //             var hour = moment.getHours();
+            //             var minute = moment.getMinutes();
+            //             var second = moment.getSeconds();
+            //             var exactTime = day+"/"+month+"/"+year+"  "+hour+":"+minute+":"+second;
+            //             var timeNum = document.getElementById('time').value;
+            //             var time = timeNum +" minutos";
+            //             $('#dialog').modal('hide');
+            //             return washingtonRef.update({
+            //                 Aceptado: true,
+            //                 Tiempo: exactTime,
+            //                 Minutos: time,
+            //             })
+            //         .then(function() {
+            //             console.log("Document successfully updated!");
+            //             document.getElementById('time').value = '';
+            //         })
+            //         .catch(function(error) {
+            //             // The document probably doesn't exist.
+            //             console.error("Error updating document: ", error);
+            //         });
+            //     });
+            // });
+            const serviceRef = db.collection("usuarios").doc(userId).collection("services").doc(serviceId)
+            now = new Date();
+            var year = moment.getFullYear();
+            var month = (moment.getMonth()+1);
+            var day = moment.getDate();
+            var hour = moment.getHours();
+            var minute = moment.getMinutes();
+            var second = moment.getSeconds();
+            var exactTime = day+"/"+month+"/"+year+"  "+hour+":"+minute+":"+second;
+            var timeNum = document.getElementById('time').value;
+            var time = timeNum +" minutos";
+            $('#dialog').modal('hide');
+            serviceRef.update({
+                aceptado: true,
+                tiempo: exactTime,
+                minutos: time
+            })
+                .then(function() {
+                    document.getElementById('time').value = ''
+                    alert("Se ha notificado al usuario")
+                })
+                .catch(function() {
+                    alert("Ocurrió un error al aceptar el viaje")
+                })
         });
-      });
+    });
 }
 
 function notify(){
